a2f72b63ae37d704c4eb49d7c4a1d307761a873c
RioShine <tnnghia1994@gmail.com>
2015-11-22 14:57:06 +0700

    1.0

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Fuck Cho Vinh");
+		System.out.println("Vinh an cut cho");
 	}
 
 }
