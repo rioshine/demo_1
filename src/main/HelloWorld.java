b81804da0e2d098f088bd35cb108b446a569524f
rioshine <rioshine2109@gmail.com>
2015-11-22 15:03:32 +0700

    Update HelloWorld.java

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Vinh an cut cho");
+		System.out.println("Vinh ngu nhu cho");
 	}
 
 }
